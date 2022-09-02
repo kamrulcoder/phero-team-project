4f290a8709edfd2410791621d9facfe4e858c89e
Shah Muhammad Jannatul Hasan <58700965+mdjannatulhasan@users.noreply.github.com>
2022-09-02 12:01:17 +0600

    Header for large screens

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
-import Header from './components/home/header/Header';
+import Header from './components/home/header-hasan/Header';
 
 function App() {
     return (
         <div className="App">
-            <Header>This is Header</Header>
+            <Header></Header>
         </div>
     );
 }
