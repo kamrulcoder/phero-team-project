378fb9dd6b620b8f084d9ee950b696bb4634ac66
Shah Muhammad Jannatul Hasan <58700965+mdjannatulhasan@users.noreply.github.com>
2022-09-02 15:34:26 +0600

    Updated Header and menu

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,14 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import CustomMenu from './components/CustomMenu/Menu';
 import Header from './components/home/header-hasan/Header';
+import Home from './pages/home/Home';
 
 function App() {
     return (
         <div className="App">
             <Routes>
+
+                <Route path="/" element={<Home />}></Route>
                 <Route path="/menu" element={<CustomMenu />}></Route>
             </Routes>
         </div>
